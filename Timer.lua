4e7f841591c496aabb15e860d5e1a2ee88f43b8f
FourTheGreat <125369353+FourTheGreat@users.noreply.github.com>
2024-02-29 10:31:27 -0300

    Update Timer.lua
    onComplete works now

--- Timer.lua
+++ Timer.lua
@@ -1,50 +1,52 @@
 luaDebugMode = true
 package.path = package.path..debug.getinfo(1,'S').source:sub(2):match('(.+scripts/)')..'?.lua;'
-require'asleep.Basic'
-Timer = Basic.extend('Timer')
+require'asleep.Types'
+require'asleep.Timer'
+require'asleep.tween.EaseFuncs'
+Tween = Timer.extend('Tween')
 
-Timer.setField('curTime',0)
-Timer.setField('maxTime',0)
-Timer.setField('loops', 1)
-Timer.setField('running', false)
-Timer.setField('onComplete',function()end)
-Timer.setField('run',function(I)
-	I.rawset('curTime', 0)
-	I.rawset('running',true)
-end)
-Timer.setField('cancel',function(I)
-	I.destroy()
-end)
-Timer.setField('pause', function(I)
-	I.rawset('running', false)
-end)
-Timer.setField('resume', function(I)
-	I.rawset('running',true)
-end)
-
-Timer.onUpdate = function(I,e)
-	if I.rawget('running') then I.rawset('curTime', I.rawget('curTime')+e) end
-	if I.rawget('curTime') >= I.maxTime and I.rawget('running') then
-		if I.rawget('loops') == -1 then
-			I.run()
-		else
-			I.rawset('loops',I.rawget('loops')-1)
-			if I.rawget('loops') > 0 then
-				I.run()
-			else
-				I.rawset('running', false)
-			end
-		end
-		I.onComplete(I.rawget('loops'))
+Tween.setField('obj', '')
+Tween.setField('vars', {})
+Tween.setField('varStarts', {})
+Tween.setField('objMode', false)
+Tween.setField('easeFunc', 'linear')
+Tween.onUpdate = function(I,e)
+	Tween.super.onUpdate(I,e)
+ for k, v in pairs(I.rawget('vars')) do
+  local o = I.rawget('obj')
+  local s = I.rawget('varStarts')
+  local f = EaseFuncs[I.rawget('easeFunc')] or EaseFuncs.linear
+  if I.rawget('objMode') then
+   o[k]=s[k]+(f(v,s[k],I.rawget('curTime')/I.rawget('maxTime')))
+   --debugPrint(k..': '..v..' '..s[k])
+  else
+   setProperty(o..'.'..k,s[k]+f(v,s[k],I.rawget('curTime')/I.rawget('maxTime')))
+  end
  end
 end
-
-Timer.setField('_update', Timer.onUpdate)
-
-Timer.new = function(time,loops,onComplete)
-	local I = Timer.createInstance()
-	I.rawset('maxTime',  time or 1)
-	I.rawset('loops',  loops or 1)
-	I.rawset('onComplete',  onComplete or function()end)
+Tween.setField('_update', Tween.onUpdate)
+Tween.new = function(obj, vars, time, ease, onComplete)
+	local I = Tween.createInstance(time,1,onComplete)
+ I.rawset('obj', obj)
+ if type(obj) ~= 'string' then--assume it's an asleep object in any case where it's not a string.
+  I.rawset('objMode',true)
+ end
+ I.rawset('vars',vars)
+ if vars.is then
+  if vars.type == 'Color' then
+   I.rawset('vars', {r=vars.r,g=vars.g,b=vars.b,a=vars.a})
+  end
+ end
+ I.rawset('easeFunc', ease or 'linear')
+ local starters = {}
+ for k, v in pairs(I.rawget('vars')) do
+  if I.rawget('objMode') then
+   starters[k] = obj.rawget(k)
+  else
+   starters[k] = getProperty(obj..'.'..k)
+  end
+ end
+ I.rawset('varStarts',starters)
+ I.run()
 	return I
 end
