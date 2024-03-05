7f6c92247f22d07bb8a0d97d5dcc84282c25c276
FourTheGreat <125369353+FourTheGreat@users.noreply.github.com>
2024-03-05 14:56:24 -0300

    Update Group.lua

--- group/Group.lua
+++ group/Group.lua
@@ -1,13 +1,24 @@
 luaDebugMode = true
-package.path = package.path..debug.getinfo(1,'S').source:sub(2):match('(.+scripts/)')..'?.lua;'
-local Basic = import'asleep.Basic'
-local Group = Basic.extend('Group')
+package.path = package.path..debug.getinfo(1,'S').source:sub(2):match('(.+asleep.)')..'?.lua;'
+require'Basic'
+Group = Basic.extend('Group')
 
 Group.setField('members',{})
-Group.setField('forEach',function(I,func)
- for k,v in pairs(I.members) do
-  func(v)
+Group.setField('foreach',function(I,func)
+ for k,v in pairs(I.rawget('members')) do
+  func(v,k)
  end
 end)
-
-return Group
+Group.setField('length', 0, function(I)return(#I.rawget('members'))end,'never')
+Group.setField('add', function(I,obj)
+ table.insert(I.rawget('members'),obj)
+end)
+Group.setField('remove', function(I,id)
+ table.remove(I.rawget('members'),id)
+end)
+Group.setField('get', function(I,id)
+ if I.rawget('members')[id] ~= nil then
+  debugPrint('bruh')
+  return I.rawget('members')[id]
+ end
+end)
